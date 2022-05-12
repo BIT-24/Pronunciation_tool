8b29a944a5b7aa32fdc456734667321fa8e70761
vishalbarot <vishalbarot@ymail.com>
2022-05-12 13:50:04 -0500

    first commit

---
+++ src/main/java/com/bit24/pronuncitation_tool/PronunciationToolApplication.java
@@ -0,0 +1,11 @@
+package com.bit24.pronuncitation_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PronunciationToolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PronunciationToolApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bit24/pronuncitation_tool/PronunciationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit24.pronuncitation_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PronunciationToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
